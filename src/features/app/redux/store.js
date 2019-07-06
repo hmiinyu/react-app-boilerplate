5c53738f67d27fd5502f5c8fe6c4912793930f84
hmiinyu <hmiinyu@gmail.com>
2019-07-06 16:21:04 +0800

    feat():remove useless comment

--- src/features/app/redux/store.js
+++ src/features/app/redux/store.js
@@ -5,8 +5,6 @@ import rootReducer from '@/features/app/redux/reducers'
 const store = ReduxFactory.createStore(rootReducer, thunk)
 const checkIsAuth = () => store.getState().auth.loginUser.authenticated
 
-// enableReduxHotUpdate(store, '@/features/app/redux/reducers', require('@/features/app/redux/reducers').default)
-
 export default {
   store,
   checkIsAuth
