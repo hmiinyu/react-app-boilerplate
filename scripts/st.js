229e4b761f8ed225806b6b1d25b0d4e2953b7fea
hmiinyu <hmiinyu@gmail.com>
2019-07-31 11:01:02 +0800

    feat(): update m2-core and m2-redux

--- scripts/st.js
+++ scripts/st.js
@@ -2,7 +2,8 @@
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'stagingment';
+process.env.NODE_ENV = 'production';
+process.env.BUILD_ENV = 'stagingment';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
